76a685cc6965efba6f73a6305424e0d8a3bd5ee2
taiga-tech <taiga.dev.n@gmail.com>
2021-03-31 03:44:44 +0900

    Update testing

---
+++ src/tests/Feature/Posts/PhotoSubmitApiTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature\Posts;
+
+use App\Models\Post;
+use App\Models\PostPhotos;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class PhotoSubmitApiTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    /**
+     * @test
+     */
+    public function should_ファイルをアップロードできる()
+    {
+        Storage::fake('s3');
+
+        $photos = [];
+        for ($i = 1; $i <= 6; $i++)
+        {
+            array_push($photos, array( 'photo' => UploadedFile::fake()->image("photo{$i}.png") ));
+        }
+
+        $response = $this
+            ->actingAs($this->user)
+            ->json('POST', route('posts.store'), $data = [
+                'nickname' => $this->faker->name,
+                'message' => $this->faker->name,
+                'files' => $photos,
+            ]);
+
+        $response->assertStatus(201);
+
+        $post = Post::first();
+
+        $this->assertEquals($data['message'], $post->message);
+        $this->assertCount(6, $post->photos);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['message' => $post->message]);
+
+        Storage::cloud()->assertExists($post->filename);
+    }
+
+    /**
+     * @test
+     */
+    public function should_データベースエラーの場合はファイルを保存しない()
+    {
+        Schema::drop('post_photos');
+
+        Storage::fake('s3');
+
+        $response = $this->actingAs($this->user)
+            ->json('POST', route('posts.store'), [
+                'nickname' => $this->faker->name,
+                'message' => $this->faker->name,
+                'files' => UploadedFile::fake()->image('photo.jpg'),
+            ]);
+
+        $response->assertStatus(500);
+
+        $this->assertEquals(0, count(Storage::cloud()->files()));
+    }
+}
